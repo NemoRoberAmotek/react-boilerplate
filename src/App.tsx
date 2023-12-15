59ccf2a97cebb29a3b166c7c4acf0542f61799d0
Nemo <nemo.rober@blue-planet.be>
2023-12-15 08:18:09 +0100

    remove NextUI provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
-import { NextUIProvider } from '@nextui-org/react'
 
 function App() {
   return (
-    <NextUIProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </NextUIProvider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   )
 }
 
