a428049311218d71d94fedc11c046530d30ae84b
Nemo <nemo.rober@blue-planet.be>
2023-12-15 08:52:53 +0100

    adds navigation and base Page

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: bold;
+`
+
+export default NavLink
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { Container } from '@mui/material'
 
 const Page = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+  return (
+    <Container fixed sx={{ mt: '4rem', py: '3rem' }}>
+      {children}
+    </Container>
+  )
 }
 
 export default Page
