f605b8e91a77ab71a9eaed4db7e83620dbc355fb
Nemo <nemo.rober@blue-planet.be>
2023-12-18 12:42:23 +0100

    adds breeds Index

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { theme } from './theme'
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
 import Navigation from './components/Navigation'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <CssBaseline />
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Navigation/>
+          <Navigation />
           <Routes />
         </BrowserRouter>
+        <ToastContainer />
       </ThemeProvider>
     </>
   )
